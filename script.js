b5dbd13b7a158d34648028e63943afdfa9b68ba4
VIctorx667 <puilletvictor@gmail.com>
2025-10-03 14:54:13 +0000

    ok

---
+++ script.js
@@ -0,0 +1,28 @@
+const prenom = "Victor"
+const nom = "Puillet"
+let age = 18 
+    //nom = "oui" //il ne peut que y avoir une seul constante dans meme nom
+
+console.log(prenom)
+console.log(nom)
+console.log(age)
+
+if (age <13) {
+    console.log("Accès interdit aux moins de 13 ans.")
+} else if (age <18) {
+    console.log("Vous pouvez accéder à la section ado.")
+} else {
+    console.log("Bienvenue dans l'espace adulte.")
+}
+
+for (let i = 0 ; i <= 10; i++) {
+    console.log("7*"+i+"="+(7*i))
+}       
+
+const calculerMoyenne = (note1, note2) => {
+    const moyenne = (note1 + note2) / 2;
+    return moyenne;
+};
+
+// Raccourci ultime si la fonction ne fait qu'un return sur une seule ligne
+const additionner = (a, b) => a + b;
